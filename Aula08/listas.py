7eecac5e140cb4622b3f3291b527fe2b50329509
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-05-13 18:30:20 -0300

    Exercícios Aula 08
    Incompleto

---
+++ Aula08/listas.py
@@ -0,0 +1,50 @@
+'''nomes = ['Paulo','Morgania','Hadassa']
+print(nomes)
+familia = "Familia Gama"
+
+for nome in nomes:
+    print("-",nome)
+
+print(len(nomes))
+
+#nomes.sort  - Ordena de forma crescente
+
+if "hadassa" in nomes:
+    print("Está")
+else:
+    print("Não")
+
+for letra in familia:
+    if letra == " ":
+       None 
+    else:
+        print(letra)'''
+
+#Exercício de Fixação
+
+#Exercicio 01
+'''frase = input("Digite uma frase: ")
+a = 0
+e = 0
+i = 0
+o = 0
+u = 0
+consoante = 0
+
+for cont in frase:
+    if cont == "a":
+        a = a+1
+    elif cont == "e":
+        e = e+1
+    elif cont == "i":
+        i = i+1
+    elif cont == "o":
+        o = o+1
+    elif cont == "u":
+        u = u+1
+    else:
+        consoante = consoante+1
+
+print(f"Sua frase tem {a} letra(s) 'a' {e} letra(s) 'e' {i} letra(s) 'i' {o} letra(s) 'o' e {u} letra(s) 'u', além de {consoante} consoantes")'''
+
+
