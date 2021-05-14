830250d7888c83a557b7585a81d0bdc17f9e395d
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-05-13 21:53:17 -0300

    Exercício Aula 08

--- Aula08/listas.py
+++ Aula08/listas.py
@@ -5,11 +5,11 @@ familia = "Familia Gama"
 for nome in nomes:
     print("-",nome)
 
-print(len(nomes))
+print(len(nomes))'''
 
 #nomes.sort  - Ordena de forma crescente
 
-if "hadassa" in nomes:
+'''if "hadassa" in nomes:
     print("Está")
 else:
     print("Não")
@@ -46,5 +46,27 @@ for cont in frase:
         consoante = consoante+1
 
 print(f"Sua frase tem {a} letra(s) 'a' {e} letra(s) 'e' {i} letra(s) 'i' {o} letra(s) 'o' e {u} letra(s) 'u', além de {consoante} consoantes")'''
+
+#Questão 2
+
+'''n1 = int(input("Digite um número: "))
+
+for cont in range(1,n1+1):
+    div = n1%cont
+    if div == 0:
+        print(cont)'''
+
+#Questão 3
+
+'''frase = input("Digite uma frase: ")
+
+for letra in frase:
+    if letra in ("aeiouAEIOU"):
+        None
+    else:
+        print(letra, end="")'''
+
+
+
 
 
