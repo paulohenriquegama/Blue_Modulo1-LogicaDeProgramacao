0bbb1a7a53d7a97c97d7ea25596a54208662a29e
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-05-21 17:30:01 -0300

    Verificação de aprendizagem concluída com sucesso.

--- Aula14/VerificaçãoDeAprendizagem.py
+++ Aula14/VerificaçãoDeAprendizagem.py
@@ -20,15 +20,15 @@ def maior40(n1,n2):
 n1 = float(input("Digite o primeiro numero: "))
 n2 = float(input("Digite o segundo numero: "))
 
-print(f"A soma dos valor é {n1+n2:.0f}")
-print(f"A multiplicão dos valor é {n1*n2:.0f}")
-print(f"A multiplicão dos valor é {n1//n2:.0f}")
+print(f"A soma dos valores é {n1+n2:.0f}")
+print(f"A multiplicão dos valores é {n1*n2:.0f}")
+print(f"A divisão dos valores é {n1//n2:.0f}")
 maior(n1,n2)
 print("O resultado da soma dos dois numeros é ", impopar(n1,n2))
 maior40(n1,n2)'''
 
 #Questão 2
-'''frase = input("Digite uma frase: ")
+'''frase = input("Digite uma frase: ").lower()
 a = 0
 e = 0
 i = 0
@@ -55,14 +55,18 @@ print(f"Sua frase tem {a} letra(s) 'a' {e} letra(s) 'e' {i} letra(s) 'i' {o} let
 print("A frase digitada sem as vogais fica:")
 for cont in frase:
     if cont not in ('aeéêiíîoóôõuúûaáâã'):
-        print(cont, end="")'''
-    
+        print(cont, end="")
+  '''  
 #Questão 3
 '''import random
 import os
 import time
 
-senha = "1a2b"
+senha = input("Cadastre uma senha para entrar no jogo: ")
+print("Senha cadastrada com sucesso!!")
+time.sleep(0.3)
+os.system('cls')
+
 resp = ""
 while resp != senha:
     resp = input("Digite a senha: ")
@@ -86,13 +90,14 @@ while resp != maquina:
         os.system('cls')
         time.sleep(1)
 
-print("Parabéns você acertou o número!!!!")'''
+print("Parabéns você acertou o número!!!!")
 
-
+'''
 #Questão 4
-
+'''
 import datetime
-opc = 0
+from time import strptime
+opc= 0
 
 def bissexto(ano):
     
@@ -151,3 +156,89 @@ while opc != 1:
     if resp in "nN":
         opc = 1
 print("Programa finalizado!!")
+'''
+# Questão 5
+'''
+def semvogais(frase):
+    vogais = 0
+    
+    imprimir = []
+    for cont in frase:
+        if cont not in ('aeéêiíîoóôõuúûaáâã'):
+            print(cont, end="")
+            imprimir.append(cont)
+        else:
+            vogais +=1
+    return imprimir,print(f'\nForam retiradas {vogais} vogais')
+        
+frase = input("Digite uma frase: ")
+print('Frase impresa sem vogais:')
+semvogais(frase)
+print()
+
+'''
+# Questão 6
+
+'''
+perguntas = [input('Telefonou para a vítima? \n')
+             ,input('Esteve no local do crime? \n')
+             ,input('Mora perto da vítima? \n')
+             ,input('Devia para a vítima? \n')
+             ,input('Já trabalhou com a vítima? \n')]
+
+
+
+sim = 0
+
+for cont in range(5):
+    if perguntas[cont] in ('sS'):
+        sim += 1
+    
+sim = perguntas.count('s')
+if sim == 2:
+    print("Suspeito")
+elif sim >2 and sim <5:
+    print("Cúmplice")
+elif sim == 5:
+    print("Culpado")
+else:
+    print("Inocente")
+
+'''
+# Questão 7
+'''
+impar = []
+par = []
+lista = [par,impar]
+cont = 0
+while cont < 7:
+    cont +=1
+    n1 = int(input(f"Digite o {cont}º numero: "))
+    if n1%2 == 0:
+        par.append(n1)
+    else:
+        impar.append(n1)
+
+par.sort()
+impar.sort()
+print(lista)
+'''
+# Questão 8
+
+import datetime
+import operator
+nome = input("Qual seu nome? ")
+anoNasc = int(input("Digite o ano de nascimento: "))
+carteira = input("Tem carteira registrada? se não digite 0: ")
+data_atual = datetime.date.today()
+ano_atual = int(data_atual.year)
+
+if carteira != '0':
+    anoContrato = int(input("Digite o ano que foi contratado: "))
+    salario = float(input("Digite o seu salario: "))
+    temp_aposentar = 35 - (ano_atual - anoContrato)
+    dic = {'nome': nome,'idade':ano_atual-anoNasc,'ano_aposentadoria': temp_aposentar+ano_atual}
+    print(f'O Seu nome é {dic["nome"]} atualmente tem {dic["idade"]} anos, você ira se aposentar em {dic["ano_aposentadoria"]} com {dic["idade"]+35} anos.')
+else:
+    dic = {'nome': nome,'idade':ano_atual-anoNasc}
+    print(f'O Seu nome é {dic["nome"]} atualmente tem {dic["idade"]} anos, você não possui carteira de trabalho.')
