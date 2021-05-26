2cb5522713735d25ddf27e7596b281d23ae1d023
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-05-26 10:59:22 -0300

    Lista de exercícios finalizada

--- Aula09/Execicios09.py
+++ Aula09/Execicios09.py
@@ -41,7 +41,35 @@ for cont in range(1,51):
     result = cont * preco
     print(f"{cont} - R$ {result:.2f}")'''
 
+# QUestão 7
+'''import os
+opc = 1
+cont = 1
+produto = []
+print("Lojas Tabajara")
+while opc == 1:
+    prod = float(input(f"Digite o valor do {cont}º produto: "))
+    produto.append(prod)
+    cont += 1
+    resp = input("Digite zero para sair e fechar a compra ou enter para continuar: ")
+    if resp == '0':
+        opc = 0
 
+cont = 1
+print('Total: ',sum(produto))
+valorRecebido = float(input("Valor recebido no pagamento: "))
+troco = valorRecebido - sum(produto)
+os.system("cls")
+print("Cupom Lojas Tabajara")
+print()
+for i in produto:
+    print(f'Produto {cont}: R$ {i}')
+    cont += 1
+print()
+print('Total: R$ ',sum(produto))
+print("Dinheiro: R$ ",valorRecebido)
+print("Troco: R$ ",troco)
+'''
 #Questão 8
 '''respostas = []
 sim = 0
@@ -73,6 +101,7 @@ else:
     print("Inocente")'''
 
 #Questão 9
+'''
 import time
 d1 = 0
 d2 = 0
@@ -88,7 +117,7 @@ for d1 in range(1,61):
                 for d5 in range(5,57):
                     for d6 in range(6,56):
                             print(d1,d2,d3,d4,d5,d6)
-                            time.sleep(0.1)
+                            time.sleep(0.1)'''
 
 #Desafio 1
 '''def func(valor):
@@ -106,7 +135,17 @@ for cont in range(1000,10000):
         
     
 
+#Desafio 2
+'''
+materias = int(input("Digite quantas materias que você possui na escola: "))
+cont = 0
+notas = []
+while cont < materias:
+    nota = float(input(f"Digite a nota da {cont+1}º materia: "))
+    notas.append(nota)
+    cont+=1
 
 
- 
-
+media = sum(notas) / len(notas)
+print(f"A média é {media:.2f}")
+'''
