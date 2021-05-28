c9030f673ac3a1babd63ec9b4e9b2e7325319607
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-05-28 18:16:02 -0300

    Update
    Foi introduzido uma trilha.

--- Projeto/DiaApocaliptico.py
+++ Projeto/DiaApocaliptico.py
@@ -1,13 +1,13 @@
 class Personagem:
-    def __init__(self,nome, dormindo=True, infectado=False,machucado = False, vida = True):
+    def __init__(self, nome, dormindo=True, infectado=False, machucado=False, vida=True):
         self.nome = nome
         self.dormindo = dormindo
         self.infectado = infectado
         self.machucado = machucado
         self.vida = vida
-    
+
     def __str__(self):
-        #return "Você está " + ("acordado" if self.dormindo else "dormindo")
+        # return "Você está " + ("acordado" if self.dormindo else "dormindo")
         if self.dormindo == False:
             dormindo = "Acordado"
         else:
@@ -27,7 +27,7 @@ class Personagem:
             infectado = "foi contaminado"
         else:
             infectado = "não foi contaminado"
-        
+
         if self.vida == True:
             return f'\033[1;32m {self.nome} está {vida} e {dormindo} {infectado} {machucado} fisicamente.\033[m'
         else:
@@ -39,30 +39,32 @@ class Relogio:
         self.dias = 1
         self.horas = 5
         self.minutos = 0
-    
+
     def __str__(self):
         return f"{self.horas:02d}:{self.minutos:02d} do dia {self.dias:02d}"
-    
+
     def avancaTempo(self, horas):
         self.horas += horas
-        if self.horas >=24:
+        if self.horas >= 24:
             self.dias += 1
             self.horas = self.horas - 24
+
 
 class Texto:
     def __init__(self):
         self.__texto = ''
         self.__estilo = "verde"
         self.__velocidade = 0.02
+
     @property
     def texto(self):
         return self.__texto
 
     @texto.setter
     def texto(self, novo_texto):
-        raise ValueError ("Impossível escrever texto diretamente, tente u outro método.")
+        raise ValueError("Impossível escrever texto diretamente, tente u outro método.")
 
-    def escreverTexto(self,texto,estilo='vermelho',velocidade = 0.02):
+    def escreverTexto(self, texto, estilo='vermelho', velocidade=0.02):
         self.__texto = texto
         self.__estilo = estilo
         self.__velocidade = velocidade
@@ -88,15 +90,14 @@ class Texto:
                 time.sleep(self.__velocidade)
 
 
-
-
 class Morto:
     texto = Texto()
+
     def __init__(self):
         self.infectado = True
         self.vida = False
 
-    def imprimir(self):    
+    def imprimir(self):
         frase = '''
             ███████████████████████████████████████████████████████████████████████████████████████████
             █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
@@ -125,13 +126,23 @@ class Morto:
             █████████████████████████████████████▄░░░░░░░░░░░▄█████████████████████████████████████████
             ████████████████████████████████████████▄▄▄▄▄▄▄████████████████████████████████████████████
         '''
-        texto.escreverTexto(frase,'vermelho',0.01)
-
+        texto.escreverTexto(frase, 'vermelho', 0.01)
 
 
 import time
-import os,sys
+import os, sys
+import pygame
+
+
+
 os.system('cls')
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("Projeto/audio.mp3")
+pygame.mixer.music.play()
+
+
+
 opc = 0
 print('''
 Loading…
@@ -165,9 +176,9 @@ print('''
 100%
 ██████████
 ''')
-
+pygame.mixer.music.stop()
 os.system('cls')
-#frase = "\033[;1mInicio do jogo!!\033[m"
+# frase = "\033[;1mInicio do jogo!!\033[m"
 frase = '''
                         ██╗███╗░░██╗██╗░█████╗░██╗░█████╗░  ██████╗░░█████╗░  ░░░░░██╗░█████╗░░██████╗░░█████╗░
                         ██║████╗░██║██║██╔══██╗██║██╔══██╗  ██╔══██╗██╔══██╗  ░░░░░██║██╔══██╗██╔════╝░██╔══██╗
@@ -177,17 +188,16 @@ frase = '''
                         ╚═╝╚═╝░░╚══╝╚═╝░╚════╝░╚═╝░╚════╝░  ╚═════╝░░╚════╝░  ░╚════╝░░╚════╝░░╚═════╝░░╚════╝░
 '''
 
-
 for i in list(frase):
     print(i, end='')
-    #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+    # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
     sys.stdout.flush()
     time.sleep(0.001)
 print()
 
 print()
 nome = input("\033[;1mDigite o nome do seu personagem: \033[m")
-p1 = Personagem(nome)   
+p1 = Personagem(nome)
 relogio = Relogio()
 morto = Morto()
 texto = Texto()
@@ -206,36 +216,36 @@ O que você irá fazer?
         \033[m''')
     escolha = input("\033[;1mDigite uma das alternativas acima: \033[m")
     if escolha == "1":
-       os.system('cls')
-       p1.infectado = True
-       p1.machucado = True
-       p1.vida = False
-       frase = f"\nVc se depara com um zumbi, e como não estava preparado ele conseguiu te atingir, vc é contaminado e após 1h morre.\n"
-       texto.escreverTexto(frase)
-       relogio.avancaTempo(1)
-       time.sleep(2)
-       
-       print(relogio)
-       print()
-       print(p1)
-       print()
-       morto.imprimir()
-       opc = 1
-       
-    
+        os.system('cls')
+        p1.infectado = True
+        p1.machucado = True
+        p1.vida = False
+        frase = f"\nVc se depara com um zumbi, e como não estava preparado ele conseguiu te atingir, vc é contaminado e após 1h morre.\n"
+        texto.escreverTexto(frase)
+        relogio.avancaTempo(1)
+        time.sleep(2)
+
+        print(relogio)
+        print()
+        print(p1)
+        print()
+        morto.imprimir()
+        opc = 1
+
+
     elif escolha == "2":
         os.system('cls')
         frase = f"\033[1;32m{p1.nome} ligou a TV e os noticiários estão informando que houve uma explosão em um laboratório e um vírus muito perigoso foi disseminado e as pessoas infectadas estão virando zumbis. \033[m"
 
         for i in list(frase):
             print(i, end='')
-            #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+            # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
             sys.stdout.flush()
             time.sleep(0.02)
         print()
 
         while opc == 0:
-            
+
             print(f'''\033[;1m
 O que vc fará agora?
 1 – Procurar uma arma para se defender e permanecer na sua residência.
@@ -252,7 +262,7 @@ O que vc fará agora?
                 p1.vida = False
 
                 for i in list(frase):
-                    print(i,end= "")
+                    print(i, end="")
                     sys.stdout.flush()
                     time.sleep(0.05)
                 print()
@@ -273,10 +283,10 @@ O que vc fará agora?
                 frase = f'\033[1;91m {p1.nome} trancou a porta do seu quarto e ficou escondido embaixo da cama. O número de zumbis crescia a cada instantes. Após 2hrs de tentativa eles conseguiram arrombar a porta e vc não teve como se defender, foi contaminado fica muito ferido e após 1hr morre.\033[m'
                 for i in list(frase):
                     print(i, end='')
-                    #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+                    # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
                     sys.stdout.flush()
                     time.sleep(0.05)
-                print()    
+                print()
                 time.sleep(2)
                 relogio.avancaTempo(3)
                 print()
@@ -285,16 +295,16 @@ O que vc fará agora?
                 print(p1)
                 print()
                 opc = 1
-           
+
             elif escolha == '3':
                 os.system('cls')
                 frase = f'\033[1;32m {p1.nome} encontrou uma arma pegou alguns mantimentos e fugiu pela janela aproveitando que não tinha nenhum zumbi por perto. Depois de 5h de caminhada em busca de ajuda, ouvi pessoas pedindo ajuda.\033[m'
                 for i in list(frase):
                     print(i, end='')
-                    #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+                    # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
                     sys.stdout.flush()
                     time.sleep(0.05)
-                print()    
+                print()
                 time.sleep(2)
                 relogio.avancaTempo(5)
                 print()
@@ -317,10 +327,10 @@ O que vc fará agora?
                         frase = f'\033[1;91m {p1.nome} Vc encontra um grupo de 3 pessoas e se junta a elas em uma batalha contra um grupo de zumbis que não para de crescer, depois de 3hrs de batalha vc e todos os seus amigos são infectados e não resistem a infecção morrendo após 1hr.\033[m'
                         for i in list(frase):
                             print(i, end='')
-                            #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+                            # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
                             sys.stdout.flush()
                             time.sleep(0.05)
-                        print()    
+                        print()
                         time.sleep(2)
                         relogio.avancaTempo(4)
                         print()
@@ -331,9 +341,10 @@ O que vc fará agora?
                         opc = 1
                     elif escolha == '2':
                         os.system('cls')
-                        print(f"\033[1;32m{p1.nome} ignorou os pedidos de ajuda, pq viu que fora do prédio havia um grupo muito grande de zumbis e que vc não teria chance de entrar.\033[m")
+                        print(
+                            f"\033[1;32m{p1.nome} ignorou os pedidos de ajuda, pq viu que fora do prédio havia um grupo muito grande de zumbis e que vc não teria chance de entrar.\033[m")
                         time.sleep(2)
-                        
+
                         print(relogio)
                         print()
                         print(p1)
@@ -343,7 +354,7 @@ O que vc fará agora?
 
 
 
-            
+
     elif escolha == "3":
         os.system('cls')
         p1.infectado = True
@@ -352,10 +363,10 @@ O que vc fará agora?
         frase = f'\033[1;91m {p1.nome} trancou a porta do seu quarto e ficou escondido embaixo da cama. O número de zumbis crescia a cada instantes. Após 2hrs de tentativa eles conseguiram arrombar a porta e vc não teve como se defender, foi contaminado fica muito ferido e após 1hr morre.\033[m'
         for i in list(frase):
             print(i, end='')
-            #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+            # O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
             sys.stdout.flush()
             time.sleep(0.05)
-        print()    
+        print()
         time.sleep(2)
         relogio.avancaTempo(3)
         print()
